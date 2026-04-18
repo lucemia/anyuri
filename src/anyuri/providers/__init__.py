9502c44a197f37c385f91da37bb656387351c007
davidchen <davidchen@davidchens-Mac-mini.local>
2026-04-18 10:20:46 +0800

    feat: export all providers from anyuri.providers

---
+++ src/anyuri/providers/__init__.py
@@ -0,0 +1,18 @@
+"""
+anyuri cloud provider URI types.
+
+Importing from this module registers the providers with AnyUri automatically:
+
+    >>> from anyuri.providers import GSUri, S3Uri
+    >>> from anyuri import AnyUri
+    >>> AnyUri("gs://bucket/key.jpg")
+    GSUri("gs://bucket/key.jpg")
+"""
+
+from anyuri.providers._gcs import GSUri
+from anyuri.providers._s3 import S3Uri
+from anyuri.providers._azure import AzureUri
+from anyuri.providers._r2 import R2Uri
+from anyuri.providers._b2 import B2Uri
+
+__all__ = ["GSUri", "S3Uri", "AzureUri", "R2Uri", "B2Uri"]
